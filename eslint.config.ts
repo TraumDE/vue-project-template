485ab99882973a69dd23ed4b2887e81de53992a2
TraumDE <eugene301workmaik@gmail.com>
2025-09-25 02:22:57 +0300

    Add composables naming check

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,7 @@
 import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
+import pluginCheckFile from 'eslint-plugin-check-file'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
@@ -19,4 +20,18 @@ export default defineConfigWithVueTs(
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
   skipFormatting,
+
+  {
+    plugins: {
+      'check-file': pluginCheckFile,
+    },
+    rules: {
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          'src/composables/**/*.ts': 'use+([A-Z])*',
+        },
+      ],
+    },
+  },
 )
