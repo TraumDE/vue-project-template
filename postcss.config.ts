35f61a9391838b8d8c89dd733963112ee47b4d46
TraumDE <eugene301workmaik@gmail.com>
2025-09-25 02:25:22 +0300

    Remove unused postcss package

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,14 +1,8 @@
 import pxToRem from 'postcss-pxtorem'
-import customMedia from 'postcss-custom-media'
-// import globalData from '@csstools/postcss-global-data'
 import type { Config } from 'postcss-load-config'
 
 const config: Config = {
   plugins: [
-    // globalData({
-    //   files: ['./src/assets/styles/helpers/_breakpoints.scss'],
-    // }),
-    customMedia(),
     pxToRem({
       propList: ['*'],
       mediaQuery: true,
