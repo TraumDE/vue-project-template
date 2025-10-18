803cd0afe98f02abe76b521132073b809d53095d
TraumDE <eugene301workmaik@gmail.com>
2025-10-18 17:01:15 +0300

    Change formatBaseLink util

--- src/utils/formatBasePath.ts
+++ src/utils/formatBasePath.ts
@@ -1,7 +1,3 @@
-const BASE_URL = import.meta.env.BASE_URL
-
-const formanBasePath = (path: string) => {
-  return BASE_URL + path.slice(1)
+export default (path: string) => {
+  return import.meta.env.BASE_URL + path.slice(1)
 }
-
-export default formanBasePath
